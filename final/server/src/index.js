43ecdba31cefc5f50215696e4ad7b308409ab082
AndreCosta101 <andre.costa101@gmail.com>
2021-04-26 13:15:22 -0300

    tutorial

--- final/server/src/index.js
+++ final/server/src/index.js
@@ -3,16 +3,16 @@ const typeDefs = require('./schema');
 const resolvers = require('./resolvers');
 
 const TrackAPI = require('./datasources/track-api');
-
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   dataSources: () => {
     return {
       trackAPI: new TrackAPI(),
-    };
-  },
-});
+    }
+  }
+})
+
 
 server.listen().then(() => {
   console.log(`
